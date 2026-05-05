03bd0ffe3af32227df086e02ceb37945502e4f22
agsastre <agsastre99@hotmail.com>
2026-05-05 15:57:10 +0200

    Update iris

--- iris.R
+++ iris.R
@@ -3,7 +3,10 @@ library(tidyverse)
 library(ggplot2)
 
 ggplot(iris, aes(Petal.Length, Sepal.Length, colour = Species)) +
-  geom_point() +
+  geom_point(alpha = 0.5) +
   geom_smooth() +
   theme_classic()
 
+ggplot(iris, aes(Petal.Length, Sepal.Length, fill = Species)) +
+  geom_boxplot(alpha = 0.7) +
+  theme_classic()
